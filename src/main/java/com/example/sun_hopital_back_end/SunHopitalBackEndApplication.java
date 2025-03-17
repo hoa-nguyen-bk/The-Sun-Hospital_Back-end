919f7c67e6ce28ddcac174df8921c03bb71b1019
hoa-minzy <hoanguyenlexuan@gmail.com>
2025-03-17 23:26:07 +0700

    first commit

---
+++ src/main/java/com/example/sun_hopital_back_end/SunHopitalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.sun_hopital_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunHopitalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunHopitalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sun_hopital_back_end/SunHopitalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sun_hopital_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunHopitalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
